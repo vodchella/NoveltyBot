31ffc3ace03a81d12ae24a223a896ef7bd88526e
vodchella <twister.kz@gmail.com>
2017-01-05 14:24:05 +0600

    В сообщении о ненайденном модуле изменил pip на pip3, т.к. у нас третий Питон

--- pkg/utils/output.py
+++ pkg/utils/output.py
@@ -31,6 +31,6 @@ def panic(msg=None):
     sys.exit(1)
 
 
-def module_not_installed(module_name, project_url, install_command="pip"):
+def module_not_installed(module_name, project_url, install_command="pip3"):
     panic("Module '%(module)s' isn't installed. Install it with 'sudo %(cmd)s install %(module)s' %(url)s\n" %
           {"module": module_name, "url": "(%s)" % project_url if len(project_url) > 0 else "", "cmd": install_command})
