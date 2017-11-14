5cbf454bbc34cf0aba557c28e50e6028092ea81b
twister <twister.kz@gmail.com>
2017-11-14 14:00:09 +0600

    Декоратор @handle_exception теперь не будет проглатывать ошибки в режиме отладки

--- pkg/utils/decorators/handle_exceptions.py
+++ pkg/utils/decorators/handle_exceptions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from cfg.defines import DEBUG
 from pkg.utils.console import get_raised_error, write_stderr
 from datetime import datetime
 
@@ -12,4 +13,7 @@ def handle_exceptions(func):
         except:
             write_stderr('[%s]: %s\n' % (datetime.now().strftime('%d.%m.%Y %H:%M:%S'),
                                          get_raised_error(full=True)))
-    return wrapped
+    if DEBUG:
+        return func
+    else:
+        return wrapped
